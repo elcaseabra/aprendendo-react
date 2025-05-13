ce8ffab6c874e2d00cce92a573e8789005657d68
elcaseabra <elkerseabra2003@gmail.com>
2025-05-13 23:50:58 +0100

    inicio do projecto

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function Meubotao(){
+  const [count, setCount] = useState(0)
+  return(
+    <button onClick={()=> setCount((antigo)=>antigo + 1)}>Eu sou um botão {count}</button>
+  );
+  
+}
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+     
+        <Meubotao/> 
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
