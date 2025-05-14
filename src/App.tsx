6196d7d13a973f35ca1522ee89e4e73f4e92e4c0
elcaseabra <elkerseabra2003@gmail.com>
2025-05-14 19:17:14 +0100

    correção no arquivo

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function Meubotao(){
   const [count, setCount] = useState(0)
   return(
-    <button onClick={()=> setCount((antigo)=>antigo + 1)}>Eu sou um botão {count}</button>
+    <button onClick={()=> setCount((antigo)=>antigo + 1)}>New button{count}</button>
   );
   
 }
